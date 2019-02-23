9d65e2305e876031fd2c87ddc1c076cc0eac761a
ooo125258 <michaelsundmlis@gmail.com>
2019-02-23 00:53:32 -0500

    q2

--- ASSIGNMENT.R
+++ ASSIGNMENT.R
@@ -4,9 +4,8 @@
 library(quantmod)
 stocks <- c("MMM","AXP","AAPL","BA","CAT","CVX","CSCO","KO","DIS","DWDP","XOM","GS","HD","IBM","INTC",
             "JNJ","JPM","MCD","MRK","MSFT","NKE","PFE","PG","TRV","UTX","UNH","VZ","V","WMT","WBA")
-
-getSymbols(stocks, src="yahoo", from="1999-12-01", to="2018-11-30")
-
+dj30 = new.env()
+getSymbols(stocks, env=dj30, src="yahoo", from="1999-12-01", to="2018-12-31", adjust=TRUE)
 #Loading functions:
 muF<-function(d,X){mean(X)*sum(d)}
 # Calculate the variance of forecaster using quadratic form
@@ -54,8 +53,10 @@ ruleReturn<-function(retX, m, r){
 all_mr <-NULL
 
 for(stock in stocks) {
-  
-  monthlyData <- monthlyReturn(get(stock), type="log")
+  stockAdjusted = dj30[[stock]][,paste(stock, ".Adjusted",sep="")]
+  monthlyData = coredata(diff(log(apply.monthly(stockAdjusted, last))))
+  monthlyData=na.omit(monthlyData)
+  #monthlyData <- monthlyReturn(get(stock), type="log")
   result<- numeric(0)
   m <- numeric(0)
   r <- numeric(0)
@@ -80,11 +81,70 @@ row.names(all_mr) <- stocks
 colnames(all_mr) <- c("m", "r")
 all_mr
 
+
+
+ew_q2 = c()
+rp_q2 = c()
+counters = c()
+for(stock in stocks) {
+  counters = c(counters, stock)
+  stockAdjusted = dj30[[stock]][,paste(stock, ".Adjusted",sep="")]
+  monthlyData = coredata(diff(log(apply.monthly(stockAdjusted, last))))
+  monthlyData=na.omit(monthlyData)
+  monthlyReturn()
+  #dj30 = new.env()
+  #getSymbols(stocks, env=dj30, src="yahoo", from="1999-12-01", to="2018-11-30", adjust=TRUE)
+  mData = coredata(monthlyData)
+  in_sample_estimate <- function(X,m, r){#X->data
+    #X <- diff(log(data))
+    d <- function(j){
+      if (j >= 0 & j <= r-1) {(m-r)*(j+1)}
+      else if (j >= r & j <= (m-1)) {r*(m-j-1)}
+    }
+    f <- function(t){
+      if (t >= m){
+        output <- 0
+        for (j in 0:(m-1)){
+          output <- output + d(j)*X[t-j]
+        }
+        output
+      }
+      else {print('t is smaller than m')}
+    }
+    #the realized return for daily data
+    re <- numeric(0)
+    for (t in (m+1):length(X)){
+      re <- c(re, sign(f(t-1))*X[t])
+    }
+    return <- sum(re)/length(m:length(X))
+    
+  }
+  #Return for weekly data
+  ew_q2=c(mean_q2,in_sample_estimate(mData,12,11)[[1]]) #This is a vector. 
+   rp_q2=c(var_q2,ruleReturn(monthlyData,12,11)[[1]])
+}
+
+perf_ew = ((12 * mean(ew_q2) - 0.02) / (sqrt(12) * sqrt(var(ew_q2))))
+perf_rp = ((12 * mean(rp_q2) - 0.02) / (sqrt(12) * sqrt(var(rp_q2))))
+
+performance
+
+
+
+
+
+
+
+
+
 #2)
 #cumulate returns
 returns <- NULL
 for(stock in stocks){
-  monthlyData <- monthlyReturn(get(stock), type="log")
+  stockAdjusted = dj30[[stock]][,paste(stock, ".Adjusted",sep="")]
+  monthlyData = coredata(diff(log(apply.monthly(stockAdjusted, last))))
+  monthlyData=na.omit(monthlyData)
+  #monthlyData <- monthlyReturn(get(stock), type="log")
   returns <- cbind(returns, monthlyData)
 }
 #R^(EW)_1 = 1/30 \Sigma_i B_(i,t-1) *  r_(i,t)
@@ -105,26 +165,44 @@ for(i in 1:nrow(returns)){
 mean(RP)
 var(RP)
 
-PART B QUESTION 1: 
+perf_ew1 = ((12 * mean(EW) - 0.02) / (sqrt(12) * sqrt(var(EW))))
+perf_rp1 = ((12 * mean(RP) - 0.02) / (sqrt(12) * sqrt(var(RP))))
+
+
+#PART B QUESTION 1: 
   
   #find delta 
   f <- function(s) {
     sum((1 - s)* s^{0:12}) - 1
   }
 
-res <- optim(0.0001, f, lower = 0)
+res <- optim(0.0001, f, lower = 0)#???????
 delta <- res$par
+delta
 
-sigmat <- NULL
+stocks <- c("MMM","AXP","AAPL","BA","CAT","CVX","CSCO","KO","DIS","DWDP","XOM","GS","HD","IBM","INTC",
+            "JNJ","JPM","MCD","MRK","MSFT","NKE","PFE","PG","TRV","UTX","UNH","VZ","V","WMT","WBA")
+dj30_last5 = new.env()
 
+getSymbols(stocks, env=dj30_last5, src="yahoo", from="2014-01-01", to="2018-12-31", adjust=TRUE)#last 5 years
+returns_last5=c()
+sigmat <- c()
+for(stock in stocks){
+  #stockAdjusted = dj30_last5[[stock]][,paste(stock, ".Adjusted",sep="")]
+  #monthlyData = coredata(diff(log(apply.monthly(stockAdjusted, last))))
+  #monthlyData=na.omit(monthlyData)
+  monthlyData <- monthlyReturn(get(stock), type="log")
+  returns_last5 <- cbind(returns_last5, monthlyData)
+}
+delta<-0.2
 #for all 30 stocks, compute its sigmat
 for(i in 1:30) {
   
   temp <- c()
   
-  for(t in 14:nrow(returns)) {
-    
-    temp <- c(temp, 13 * sum((1 - delta)*delta^{0:12}*(returns[(t-1):(t-13),i] - sum(1 - delta^{0:12} * returns[(t-1):(t-13),i]))^2))
+  for(t in 13:nrow(returns_last5)) {#13 or 14?
+    #change to 12
+    temp <- c(temp, 12 * sum((1 - delta)*delta^{0:11}*(returns_last5[(t-1):(t-12),i] - sum(1 - delta^{0:11} * returns_last5[(t-1):(t-12),i]))^2))
     
   }
   
@@ -133,7 +211,7 @@ for(i in 1:30) {
 
 sigmat
 
-#QUESTION 2 
+#b-QUESTION 2 
 #Predictive regression 
 #construct rs, t/sigma s, t-1 matrix
 
@@ -141,7 +219,7 @@ rst_sigmast <- NULL
 
 for(i in 1:30) {
   
-  rst_sigmast <- cbind(rst_sigmast, returns[14:nrow(returns),i]/sigmat[,i])
+  rst_sigmast <- cbind(rst_sigmast, returns_last5[13:nrow(returns_last5),i]/sigmat[,i])
   
   
 }
@@ -153,7 +231,7 @@ for(i in 1:30) {
   rh <- numeric(12)
   for(h in 1:12) {
     y <- rst_sigmast[(h+1):nrow(rst_sigmast),i]
-    x <- sign(returns[(h+1):nrow(rst_sigmast),i])
+    x <- sign(returns_last5[(h+1):nrow(rst_sigmast),i])
     model <- lm(y ~ x)
     rh[h] <- summary(model)$r.squared
   }
@@ -166,21 +244,24 @@ optimal_h_30
 
 #Question 3
 #TSMOM
-TSMOM <- numeric(nrow(returns) - 14)
-for(i in 14:(nrow(returns))) {
+TSMOM <- numeric(nrow(returns_last5) - 12)
+for(i in 13:(nrow(returns_last5))) {
   #weights
   ws <- numeric(30)
   for(j in 1:30) {
     #assume hs = 12 for all stocks
-    ws[j] <- sign(returns[(i -12),j])* 40/100 * sigmat[i-13,j]
+    weights[j] <- sign(returns_last5[(i -12),j])* 40/100 * sigmat[i-12,j]
   }
   
-  TSMOM[i] <- 1/30 * sum(ws * returns[i,], na.rm = TRUE)
+  TSMOM[i] <- 1/30 * sum(weights * returns_last5[i,], na.rm = TRUE)
 }
 #performances mean and vavriance of TSMOM portfolio
 mean(TSMOM)
 var(TSMOM)
-
-
+perf_tsmom = ((12 * mean(TSMOM) - 0.02) / (sqrt(12) * sqrt(var(TSMOM))))
+perf_tsmom
 #PARTC
+#Question changed:
+#The same as the 1st question.
+#The formula of ERT is in the discussion
 
